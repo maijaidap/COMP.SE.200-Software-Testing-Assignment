3111965f9dc18f0cd9d388754d07e83ba4d6274c
laurapihamaa <laura.pihamaa@kolumbus.fi>
2022-11-25 18:17:20 +0200

    lcov-file added to package.yml

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -1,5 +1,5 @@
-//const add = require("../add");
+const add = require("../add");
 
-//test("returns 3 for 1 and 2", () => {
-//    expect(add(1,2).toBe(3));
-//});
+test("returns 3 for 1 and 2", () => {
+   expect(add(1,2).toBe(3));
+});
