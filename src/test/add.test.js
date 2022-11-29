d4cd19ea7c45149c7617b0a9d82afbe9d2baffbc
laurapihamaa <laura.pihamaa@kolumbus.fi>
2022-11-29 10:25:47 +0200

    babel-config added to run test

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -1,5 +1,6 @@
-const add = require("../add");
+import '@testing-library/jest-dom'
+import add from '../add'
 
 test("returns 3 for 1 and 2", () => {
-   expect(add(1,2).toBe(3));
+   expect(add(1,2)).toBe(3);
 });
