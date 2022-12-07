35cf7fe5bb56546dcdacd10d60a9d6c4b4560214
Maija Pietilä <pietilamai@gmail.com>
2022-12-07 21:36:59 +0200

    Add tests for filter.js

---
+++ src/test/filter.test.js
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom'
+import filter from '../filter'
+
+const vegetables = [
+    { 'vegetable': 'potato', 'isRed': false },
+    { 'vegetable': 'tomato',   'isRed': true },
+    { 'vegetable': 'cucumber', 'isRed': false} ]
+
+it('filter array of string that has a certain value, should return array of filtered items', () => { 
+    expect(filter(vegetables, ({ isRed }) => isRed)).toEqual( [{"isRed": true, "vegetable": "tomato"}])
+});
+
+it('filter array of string that is null, should return empty array', () => { 
+    expect(filter([])).toEqual([[]])
+});
+
+it('filter array of string that is null, should return empty array', () => { 
+    expect(filter(null)).toEqual([[]])
+});
